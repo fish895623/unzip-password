1e37d276432c6327af89ee91763d31e1f332a9a2
fish895623 <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>
2022-02-27 00:21:36 +0900

    product repeat

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import gc
 import time
 import zipfile
 import zlib
+from itertools import product
+
+gc.set_threshold(1000, 100, 100)
 
 start_time = time.time()
 
@@ -28,25 +32,36 @@ class ZipCrack:
         """Start Crack
         :return: Password of zip file
         """
-        _try = 0
+        _length = 2
         while True:
-            try:
-                if (
-                    self.zip_file.extractall(
-                        path=self.path,
-                        pwd=str(_try).encode("utf-8"),
-                    )
-                    is None
-                ):
-                    return str(_try)
-            except RuntimeError:
-                pass
-            except zlib.error:
-                pass
-            _try += 1
+            for length in range(1, _length):
+                to_attempt = product(SET, repeat=length)
+                _length += 1
+                for attempt in to_attempt:
+                    a = "".join(attempt)
+                    print(a)
+                    try:
+                        if (
+                            self.zip_file.extractall(
+                                path=self.path,
+                                pwd=a.encode("utf-8"),
+                            )
+                            is None
+                        ):
+                            return a
+                    except RuntimeError:
+                        pass
+                    except zlib.error:
+                        pass
+                    except zipfile.BadZipfile:
+                        pass
 
 
 if __name__ == "__main__":
     app = ZipCrack(filename="zipCrack.zip", path="output")
     password = app.run()
-    print(password)
+    # print(password)
+    # for length in range(1, 8):  # only do lengths of 1 + 2
+    #     to_attempt = product(SET, repeat=length)
+    #     for attempt in to_attempt:
+    #         print("".join(attempt))
